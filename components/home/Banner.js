d64891211652a8dd9fa88e6ab2f24d36f84cd2bd
pradip <variyapradip1311996@gmail.com>
2026-05-13 21:52:38 +0530

    banner issue resolve

--- components/home/Banner.js
+++ components/home/Banner.js
@@ -1,50 +1,110 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+
 import Image from 'next/image';
+
 import { motion } from 'framer-motion';
 
+import { db } from '@/firebase/firebase';
+
+import {
+    doc,
+    getDoc
+} from 'firebase/firestore';
+
 export default function Banner() {
+
+    const [banner, setBanner] = useState(null);
+
+    useEffect(() => {
+
+        const fetchBanner = async () => {
+
+            try {
+
+                const docRef = doc(
+                    db,
+                    'homepage',
+                    'banner'
+                );
+
+                const docSnap = await getDoc(docRef);
+
+                if (docSnap.exists()) {
+
+                    console.log(
+                        'Firebase Data:',
+                        docSnap.data()
+                    );
+
+                    setBanner(docSnap.data());
+
+                }
+
+            } catch (error) {
+
+                console.log(error);
+
+            }
+        };
+
+        fetchBanner();
+
+    }, []);
+
     return (
         <section className="main_banner">
+
             {/* Banner Image */}
-            <Image
-                src="/images/banner2.png"
-                alt="soap banner"
-                width={1920}
-                height={900}
-                priority
-                className="banner_img"
-            />
+            {banner?.image && (
+
+                <Image
+                    src={banner?.image}
+                    alt="soap banner"
+                    width={1920}
+                    height={900}
+                    priority
+                    className="banner_img"
+                />
+
+            )}
 
             {/* Banner Content */}
             <motion.div
                 initial={{ opacity: 0, y: 80 }}
                 animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.8, ease: 'easeOut' }}
+                transition={{
+                    duration: 0.8,
+                    ease: 'easeOut'
+                }}
                 className="main_banner-content"
             >
+
                 <h1>
-                    Handcrafted Herbal Soaps for Gentle,
-                    Natural Skin Care
+                    {banner?.title}
                 </h1>
 
                 <p>
-                    A collection of handcrafted herbal soaps
-                    for radiant, healthy skin
+                    {banner?.subtitle}
                 </p>
 
                 <button>
+
                     Discover Your Glow
 
                     <Image
-                        className="arrow_icon"
                         src="/images/right-arrow.png"
                         alt="arrow icon"
                         width={18}
                         height={18}
+                        className="arrow_icon"
                     />
+
                 </button>
+
             </motion.div>
+
         </section>
     );
 }
---
+++ firebase/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDyv6Ypez2_0nnkvTn237qE8Pq1B4x1Uns",
+    authDomain: "pureleaf-soap-7de30.firebaseapp.com",
+    databaseURL: "https://pureleaf-soap-7de30-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "pureleaf-soap-7de30",
+    storageBucket: "pureleaf-soap-7de30.firebasestorage.app",
+    messagingSenderId: "480155350260",
+    appId: "1:480155350260:web:4d8414c8301ba250131aa1",
+    measurementId: "G-13B54J2LRH"
+  };
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
